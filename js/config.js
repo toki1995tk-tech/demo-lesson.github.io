9dbd7ab92546c586a8472d912433c919011c46ee
toki1995tk-tech <toki1995.tk@gmail.com>
2026-08-27 14:35:14 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = 'https://wbvvhlrrutmjawrwzwhk.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_QJsp3Dn1V4bxHP1DmPLlLA_coWw9tDX';
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
+
+const EVENT_KEY = 'future-school-main';
