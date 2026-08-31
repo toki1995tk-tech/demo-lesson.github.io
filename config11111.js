bdf220765191b7081370c3b60cfb4f4df5b77267
toki1995tk-tech <toki1995.tk@gmail.com>
2026-08-31 14:02:26 +0500

    Update config11111.js

--- config11111.js
+++ config11111.js
@@ -1,4 +1,4 @@
 // ВСТАВЬТЕ СЮДА ДАННЫЕ ИЗ SUPABASE:
 // Project Settings → Data API
-window.SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
+window.SUPABASE_URL = "https://wbvvhlrrutmjawrwzwhk.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_QJsp3Dn1V4bxHP1DmPLlLA_coWw9tDX";
